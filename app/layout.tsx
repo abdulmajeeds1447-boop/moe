8cfc8d7b03bed466a852798c344713bc5a30374e
abdulmajeeds1447-boop <abdulmajeed.s1447@gmail.com>
2026-01-23 20:10:23 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css"; // تأكد أن ملف الـ CSS لديك بهذا الاسم، أو غيره إلى index.css إذا كان ذلك هو اسم ملفك
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "نظام تقييم الأداء الوظيفي",
+  title: "نظام تقييم الأداء",
   description: "نظام ذكي لتقييم المعلمين",
 };
 
